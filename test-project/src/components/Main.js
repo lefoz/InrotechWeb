6d19778a5a82ffb8e21fb1935a2b928d449f305d
Martin Jensby <lefoz.jensby@gmail.com>
2017-05-04 09:55:35 +0200

    boxses working

--- test-project/src/components/Main.js
+++ test-project/src/components/Main.js
@@ -34,12 +34,17 @@ class Main extends Component {
           Top Menu
         </div>
         <div className="content-area">
+        <div className="box-left">  
         <div className="video-area">Video</div>
         <div className="graph-area">graph</div>
         <div className="task-area">taskmanager</div>
+        </div>
+        <div className="box-right">
+        <div className="index-area">index</div>
+        </div>
           {this.state.value}
         </div>
-         <div className="index-area">index</div>
+        
       </div>
     );
   }
