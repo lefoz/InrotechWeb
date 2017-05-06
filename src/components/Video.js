453dad4847ec751bd3d9892fadb93f5cf0090238
ole scharling <scharling@mensa.dk>
2017-05-06 21:39:14 +0200

    indsat video element med src, men det virker ikke endnu, og understøttes ikke i chrome.

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,9 +4,9 @@ import '../styles/Video-style.css';
 class Video extends Component {
   render() {
     return (
-       <div id="rtsp_movie">
-
-</div>
+       <div className='video_container'>
+        <embed className='vlc_player' type="application/x-vlc-plugin" pluginspage="rtsp://admin:Inrotech@192.168.1.64:554/Streaming/Channels/102/"/>
+      </div>
     );
   }
 }
