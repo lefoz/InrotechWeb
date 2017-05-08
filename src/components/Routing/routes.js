864989276b824de1ce743ef4f164ca5bc5ff2e8c
Madsen88 <kasper.madsen88@gmail.com>
2017-05-08 22:38:30 +0200

    Graph indsat og virker

--- src/components/Routing/routes.js
+++ src/components/Routing/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Login from '../Login'
 import Settings from '../Settings'
 import Main from '../Main'
+import Graph from '../Graph'
 import RobotSelector from '../RobotSelector'
 import NotFound from '../NotFound'
 
@@ -19,6 +20,7 @@ class Routes extends Component {
                     <Switch>
                         <Route exact path="/" component={Login} />
                         <Route path="/main" component={Main} />
+                        <Route path="/Graph" component={Graph} />
                         <Route path="/settings" component={Settings} />
                         <Route path="/robotselector" component={RobotSelector} />
                         <Route component={NotFound} />
