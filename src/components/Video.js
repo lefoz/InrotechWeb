b4fec8db7a84dd795ce943a72478ce25f5eba33b
ole scharling <scharling@mensa.dk>
2017-05-08 11:37:47 +0200

    ny video

--- src/components/Video.js
+++ src/components/Video.js
@@ -3,11 +3,13 @@ import '../styles/Video-style.css';
 
 //https://wiki.videolan.org/Documentation:WebPlugin/
 
+//https://github.com/Streamedian/html5_rtsp_player/wiki/HTML5-RTSP-Player
+
 class Video extends Component {
   render() {
     return (
        <div className='video_container'>
-       <object classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921" codebase="http://download.videolan.org/pub/videolan/vlc/last/win32/axvlc.cab" id="vlc">
+{/*       <object classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921" codebase="http://download.videolan.org/pub/videolan/vlc/last/win32/axvlc.cab" id="vlc">
 <embed 
     type="application/x-vlc-plugin" 
     pluginspage="http://www.videolan.org" 
@@ -17,7 +19,17 @@ class Video extends Component {
     target="rtsp://admin:Inrotech@192.168.1.64:554/Streaming/Channels/102/"
     id="vlc">
 </embed>
+</object>*/}
+<object classid="clsid:9BE31822-FDAD-461B-AD51-BE1D1C159921"
+codebase="http://downloads.videolan.org/pub/videolan/vlc/latest/win32/axvlc.cab"
+id="vlc" events="True">
+<param name="Src" value="rtsp://admin:Inrotech@192.168.1.64:554/Streaming/Channels/101" />
+<param name="ShowDisplay" value="True" />
+<param name="AutoLoop" value="False" />
+<param name="AutoPlay" value="True" />
 </object>
+
+
       </div>
     );
   }
