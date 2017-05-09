18838ff4c09274e22e358220000c9e611902d587
Martin Jensby <lefoz.jensby@gmail.com>
2017-05-09 12:01:52 +0200

    Drop Succes!

--- test-project/src/components/RobotSelector.js
+++ test-project/src/components/RobotSelector.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import '../styles/RobotSelector-style.css';
+import Dropdown from './SelectDropdown.js'
 import logo from '../images/Inrotechlogo_White_Cyan.png';
+import '../styles/Dropdown-style.css';
 
 class RobotSelector extends Component {
   render() {
@@ -14,10 +16,15 @@ class RobotSelector extends Component {
           <div>
             <div className="divContent">
               <h2>PLEASE SELECT ROBOT</h2>
-                    dropdown here
-              <h2>OR</h2>
+              <Dropdown/>
+              <Link to="/Main">
+                <input type="submit" value="CONNECT" />
+              </Link>
+              <h2>OR CONNECT MANUALLY</h2>
               <input className="roboIP" placeholder="ROBOT IP"></input><br />
               <input className="camIP" placeholder="CAMERA IP"></input><br />
+              <input className="camUserId" placeholder="CAMERA USERNAME"></input><br />
+              <input className="camPassword" placeholder="CAMERA PASSWORD"></input><br />
               <Link to="/Main">
                 <input type="submit" value="CONNECT TO ROBOT" />
               </Link>
