fe9923ef1a4e8d5ec49dc5a5836ecac23078631e
ole scharling <scharling@mensa.dk>
2017-05-16 14:04:39 +0200

    rettet proxy opsætning

--- scripts/start.js
+++ scripts/start.js
@@ -266,31 +266,11 @@ function runDevServer(host, port, protocol) {
         changeOrigin: true,
         secure: false
       },
-      "/api/values/1": {
-        target: "http://localhost:53982",
-        changeOrigin: true,
-        secure: false
-      },
-      "/api/values/2": {
-        target: "http://localhost:53982",
-        changeOrigin: true,
-        secure: false
-      },
       "/api/user": {
           target: "http://localhost:53982",
           changeOrigin: true,
           secure: false
-      },
-      "/api/user/userid": {
-          target: "http://localhost:53982",
-          changeOrigin: true,
-          secure: false
-      },
-      "/api/values/getarray/1": {
-        target: "http://localhost:53982",
-        changeOrigin: true,
-        secure: false
-      } 
+      }
     }
   });
 
